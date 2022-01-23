29c28b097e86218c5a10fc527e0beb886571ca8b
999hyeon <999@hyeon.pro>
2022-01-23 14:06:56 +0900

    :construction: container types

--- src/views/components/modules/BillForm/container.tsx
+++ src/views/components/modules/BillForm/container.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addBill } from '@store/bill';
+import type IBill from '@model/Bill';
+
 import BillForm from './index';
 
 export default function BillFormContainer() {
   const dispatch = useDispatch();
-  const onSubmit = (values: any) => {
+  const onSubmit = (values: IBill) => {
     dispatch(addBill(values));
   };
 
